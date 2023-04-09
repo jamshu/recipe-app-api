5d277ddaabf6d10b0e59b860dbb6bbb4089fd72d
Jamshid K <jamshid.k@zeeve.io>
2023-04-09 15:12:42 +0530

    whitespace remove

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,8 +6,8 @@ from django.test import SimpleTestCase
 from app import calc
 
 
-class CalcTestCase(SimpleTestCase): 
-    """Test Calculator module """
+class CalcTestCase(SimpleTestCase):
+    """ Tests Calc"""
 
     def test_add_number(self):
         res = calc.add(5, 8)
