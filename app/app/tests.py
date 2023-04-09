8fe7216faf10035f0c1a617e353ac06b85d26d07
Jamshid K <jamshid.k@zeeve.io>
2023-04-09 14:40:06 +0530

    test added

---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+Samples Tests 
+"""
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTestCase(SimpleTestCase):
+    """Test Calculator module """
+
+    def test_add_number(self):
+        res = calc.add(5,8)
+        self.assertEqual(res,13)
+
+    def test_substract_number(self):
+        res = calc.subtract(10,8)
+        self.assertEqual(res,2)
