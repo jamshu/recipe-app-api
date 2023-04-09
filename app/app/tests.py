1ab345e70bc5c2486f51c8df81f1aae9a77215ed
Jamshid K <jamshid.k@zeeve.io>
2023-04-09 15:06:07 +0530

    whitespace

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,16 +1,18 @@
 """
-Samples Tests 
+Samples Tests
+
 """
 from django.test import SimpleTestCase
 from app import calc
 
-class CalcTestCase(SimpleTestCase):
+
+class CalcTestCase(SimpleTestCase): 
     """Test Calculator module """
 
     def test_add_number(self):
-        res = calc.add(5,8)
-        self.assertEqual(res,13)
+        res = calc.add(5, 8)
+        self.assertEqual(res, 13)
 
     def test_substract_number(self):
-        res = calc.subtract(10,8)
-        self.assertEqual(res,2)
+        res = calc.subtract(10, 8)
+        self.assertEqual(res, 2)
